e6e729fe38a77b46058300f193bd8969145834f8
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-06-01 17:31:10 +0200

    file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
--- create database hbnb_dev_db if not exists
--- create user hbnb_dev with pwd hbnb_dev_pwd
--- grant hbnb_dev select on performance_schema
--- grant all privileges on hbnb_dev_db
+-- Creates the database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- add the user hbnb_dev identified with password 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- add privileges to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- refresh to apply privileges
+-- FLUSH PRIVILEGES;
