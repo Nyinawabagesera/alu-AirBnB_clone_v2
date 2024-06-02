6c14f1ac6cb2f3f3a4a0303c267971f368160afc
nitekar <139794767+nitekar@users.noreply.github.com>
2024-06-02 21:04:21 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python3
-"""This is the amenity class"""
+#!/usr/bin/python
+""" holds class Amenity"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.place import place_amenity
+import models
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
-    
+    """Representation of Amenity """
+    if models.storage_type == "db":
+        __tablename__ = "amenities"
+
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("PlaceAmenity",
+                                       backref="amenities",
+                                       cascade="delete")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
