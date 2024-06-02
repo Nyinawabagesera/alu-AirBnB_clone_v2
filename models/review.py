8cf9fd091a31d2e7f79d2a1004075bc634465a1c
nitekar <139794767+nitekar@users.noreply.github.com>
2024-06-02 21:10:51 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,19 +1,26 @@
-#!/usr/bin/python3
-"""This is the review class"""
-from sqlalchemy.ext.declarative import declarative_base
+#!/usr/bin/python
+""" holds class Review"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+import models
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    
+    """Representation of Review """
+    if models.storage_type == "db":
+        __tablename__ = "reviews"
+
+        place_id = Column(String(60),
+                          ForeignKey("places.id"))
+        user_id = Column(String(60),
+                         ForeignKey("users.id"))
+        text = Column(String(1024),
+                      nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
