e48b5d4e31f9fccebe916b70b5065e1ed4105abb
nitekar <139794767+nitekar@users.noreply.github.com>
2024-06-02 21:12:36 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""User class"""
+""" holds class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+import models
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
+    """Representation of a user """
+    if models.storage_type == "db":
+        __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship(
-        'Place',
-        backref='user',
-        cascade='all, delete-orphan')
-    reviews = relationship(
-        'Review',
-        backref='user',
-        cascade='all, delete-orphan')
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="delete")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
