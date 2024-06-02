5c13911b2e6c29aca2e31e25796c47e738079fa7
nitekar <139794767+nitekar@users.noreply.github.com>
2024-06-02 20:41:00 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,6 @@
--- Creates the database hbnb_test_db
+-- creates the database hbnb_test_db in MySQL server
+-- only if database doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- add the user hbnb_test identified with password 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- add privileges to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
--- refresh to apply privileges
--- FLUSH PRIVILEGES;
