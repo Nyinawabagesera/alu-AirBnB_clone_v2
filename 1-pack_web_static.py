705226cc37f188f23f9fd6ef7557180d272cde5e
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-06-11 14:19:19 +0200

    file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,53 +1,23 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Generate .tgz file from the contents of the web_static folder"""
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
 
-from fabric import api
+
+from fabric.operations import local
 from datetime import datetime
-import os
 
 
 def do_pack():
-    """Function to create tarball of webstatic files from the web_static
-    folder in Airbnb_v2.
-
-    Returns: path of .tgz file on success, None otherwise
-    """
-    with api.settings(warn_only=True):
-        isdir = os.path.isdir('versions')
-        if not isdir:
-            mkdir = api.local('mkdir versions')
-            if mkdir.failed:
-                return None
-        suffix = datetime.now().strftime('%Y%m%d%M%S')
-        path = 'versions/web_static_{}.tgz'.format(suffix)
-        tar = api.local('tar -cvzf {} web_static'.format(path))
-        if tar.failed:
-            return None
-        size = os.stat(path).st_size
-        print('web_static packed: {} -> {}Bytes'.format(path, size))
-        return path
-=======
-"""
-Fabric script to genereate tgz archive
-execute: fab -f 1-pack_web_static.py do_pack
-"""
-
-from datetime import datetime
-from fabric.api import *
-
-
-def do_pack():
-    """
-    making an archive on web_static folder
-    """
-
-    time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
-    else:
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local(
+        "tar -cvzf versions/web_static_{}.tgz web_static".format(
+            datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+        ),
+        capture=True,
+    )
+    if result.failed:
         return None
->>>>>>> c3ede153b1416943e2dccfe0155f6e637b1cda05 
+    return result
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,78 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Tar, transfer, transfer, and deploy static html to webservers"""
-
-from fabric import api, decorators
-from fabric.contrib import files
-from datetime import datetime
-import os
-
-api.env.hosts = ['holberton1', 'holberton3']
-api.env.hosts = ['142.44.167.235', '144.217.246.199']
-api.env.user = 'ubuntu'
-api.env.key_filename = '~/.ssh/holberton'
-
-
-def deploy():
-    """Wrapper function to pack html files into tarball and transfer
-    to web servers."""
-    return do_deploy(do_pack())
-
-
-@decorators.runs_once
-def do_pack():
-    """Function to create tarball of webstatic files from the web_static
-    folder in Airbnb_v2.
-
-    Returns: path of .tgz file on success, False otherwise
-    """
-    with api.settings(warn_only=True):
-        isdir = os.path.isdir('versions')
-        if not isdir:
-            mkdir = api.local('mkdir versions')
-            if mkdir.failed:
-                return False
-        suffix = datetime.now().strftime('%Y%m%d%M%S')
-        path = 'versions/web_static_{}.tgz'.format(suffix)
-        tar = api.local('tar -cvzf {} web_static'.format(path))
-        if tar.failed:
-            return False
-        size = os.stat(path).st_size
-        print('web_static packed: {} -> {}Bytes'.format(path, size))
-        return path
-
-
-def do_deploy(archive_path):
-    """Function to transfer `archive_path` to web servers.
-
-    Args:
-        archive_path (str): path of the .tgz file to transfer
-
-    Returns: True on success, False otherwise.
-    """
-    if not os.path.isfile(archive_path):
-        return False
-    with api.cd('/tmp'):
-        basename = os.path.basename(archive_path)
-        root, ext = os.path.splitext(basename)
-        outpath = '/data/web_static/releases/{}'.format(root)
-        try:
-            putpath = api.put(archive_path)
-            if files.exists(outpath):
-                api.run('rm -rdf {}'.format(outpath))
-            api.run('mkdir -p {}'.format(outpath))
-            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
-            api.run('rm -f {}'.format(putpath[0]))
-            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
-            api.run('rm -rf {}/web_static'.format(outpath))
-            api.run('rm -rf /data/web_static/current')
-            api.run('ln -s {} /data/web_static/current'.format(outpath))
-            print('New version deployed!')
-        except:
-            return False
-        else:
-            return True
-=======
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
@@ -81,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.91.90.7', '54.227.52.156']
+env.hosts = ['54.163.51.101', '3.87.145.187']
 
 
 def do_pack():
@@ -124,4 +50,3 @@ def deploy():
     if archive_path is None:
         return False
     return do_deploy(archive_path)
->>>>>>> c3ede153b1416943e2dccfe0155f6e637b1cda05
