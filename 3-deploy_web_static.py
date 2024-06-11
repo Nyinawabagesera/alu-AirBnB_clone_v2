ecf547f5bacc794b9e8c7cd26fa9a0bce9b3fa5f
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-06-11 12:39:21 +0000

    web

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.91.90.7', '54.227.52.156']
+env.hosts = ['54.163.51.101', '3.87.145.187']
 
 
 def do_pack():
